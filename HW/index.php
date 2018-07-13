221f4b587ed7ebb08b0d300df884aba08820ae22
Stefan Kukurekovic <40946695+StefanKukurekovic@users.noreply.github.com>
2018-07-13 16:22:47 +0200

    HW created

---
+++ HW/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Form</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>">
+</head>
+<body>
+	<div id="side">
+		<div class="select">
+			<p>SELECT</p>
+		</div>
+
+		<div class="insert">
+			<p>INSERT</p>
+		</div>
+
+		<div class="delete">
+			<p>DELETE</p>
+		</div>
+	</div>
+
+	<div id="wrap">
+		<p>Test Wrap</p>
+	</div>
+
+</body>
+</html>
